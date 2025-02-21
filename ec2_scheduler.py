ae486cdf9803081ed62596e9dfdebf18de901416
ctaminian <ctaminian@gmail.com>
2025-02-21 12:01:41 -0800

    Add a main menu with choices to start, stop and describe and EC2 isntance.

--- ec2_scheduler.py
+++ ec2_scheduler.py
@@ -13,12 +13,46 @@ EC2_INSTANCE_ID = os.getenv("EC2_INSTANCE_ID")
 
 # Create an EC2 client
 ec2 = boto3.client("ec2", aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY, region_name=AWS_REGION)
-print("EC2 client created successfully")
+print("EC2 client created successfully\n")
 
 def main():
-    # Call of the describe instance function
-    describe_instance()
 
+    # Run the main menu in a loop
+    while True:
+        run_main_menu()
+
+# Function to display the main menu and handle user input
+def run_main_menu():
+    print("Welcome to the EC2 Scheduler!")
+    print("1. Start EC2 Instance")
+    print("2. Stop EC2 Instance")
+    print("3. Describe EC2 Instance")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        start_instance()
+    elif choice == "2":
+        stop_instance()
+    elif choice == "3":
+        describe_instance()
+    elif choice == "4":
+        print("Exiting...")
+        exit()
+    else:
+        print("Invalid choice. Please try again.\n")
+        run_main_menu()
+
+# Function to start the EC2 instance
+def start_instance():
+    print("Starting EC2 instance...\n")
+
+# Function to stop the EC2 instance
+def stop_instance():
+    print("Stopping EC2 instance...\n")
+
+# Function to describe the EC2 instance
 def describe_instance():
     print("Describing EC2 instance...\n")
     # Describe the instance
@@ -38,7 +72,7 @@ def describe_instance():
     print(f"Instance ID: {instance_id}")
     print(f"Instance State: {instance_state}")
     print(f"Instance Type: {instance_type}")
-    print(f"Public IP: {public_ip}")
+    print(f"Public IP: {public_ip}\n")
 
 if __name__ == "__main__":
     main()
